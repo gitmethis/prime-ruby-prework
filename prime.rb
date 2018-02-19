caa7d0ad102f0c4b07d4979c329f765bc53a4210
Chase <chaseit12@gmail.com>
2018-02-19 21:55:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,13 +4,17 @@
 def prime?(int)
    
    i = 1
+   e = -1
    
-   while i < int
-     if int % i == 0
-        return false 
+   if int > -1
+     while i < int
+       if int % i == 0
+          return false 
+       end
+       i+=1
      end
-     i+=1
    end
+   
    
    true
 end
